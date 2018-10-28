fa483244dc7b09e34320175aa8b9d6d96c6530d6
Sean Lim <lim_kai_xiang_sean@s2013.sst.edu.sg>
2018-10-28 15:18:29 +0800

    Add input implementation for controllers

--- include/input.h
+++ include/input.h
@@ -1,4 +1,193 @@
 #ifndef _INPUT_H
 #define _INPUT_H
 
+#include <windows.h>
+#include <windowsX.h>
+#include <string>
+#include <XInput.h>
+
+#include "constants.h"
+#include "gameError.h"
+
+// High-definition mouse
+#ifndef HID_USAGE_PAGE_GENERIC
+#define HID_USAGE_PAGE_GENERIC ((USHORT)0x01)
+#endif
+#ifndef HID_USAGE_GENERIC_MOUSE
+#define HID_USAGE_GENERIC_MOUSE ((USHORT)0x02)
+#endif
+
+namespace inputNS
+{
+const int KEYS_ARRAY_LEN = 256;
+
+const UCHAR KEYS_DOWN = 1;
+const UCHAR KEYS_PRESSED = 2;
+const UCHAR MOUSE = 4;
+const UCHAR TEXT_IN = 8;
+
+const UCHAR KEYS_MOUSE_TEXT = KEYS_DOWN + KEYS_PRESSED + MOUSE + TEXT_IN;
+
+} // namespace inputNS
+
+const DWORD GAMEPAD_THUMBSTICK_DEADZONE = (DWORD)(0.20f * 0x7FFF);
+const DWORD GAMEPAD_TRIGGER_DEADZONE = 30;
+const DWORD MAX_CONTROLLERS = 4;
+
+// Bit corresponding to gamepad button in state.Gamepad.wButtons
+const DWORD GAMEPAD_DPAD_UP = 0x0001;
+const DWORD GAMEPAD_DPAD_DOWN = 0x0002;
+const DWORD GAMEPAD_DPAD_LEFT = 0x0004;
+const DWORD GAMEPAD_DPAD_RIGHT = 0x0008;
+const DWORD GAMEPAD_START_BUTTON = 0x0010;
+const DWORD GAMEPAD_BACK_BUTTON = 0x0020;
+const DWORD GAMEPAD_LEFT_THUMB = 0x0040;
+const DWORD GAMEPAD_RIGHT_THUMB = 0x0080;
+const DWORD GAMEPAD_LEFT_SHOULDER = 0x0100;
+const DWORD GAMEPAD_RIGHT_SHOULDER = 0x0200;
+const DWORD GAMEPAD_A = 0x1000;
+const DWORD GAMEPAD_B = 0x2000;
+const DWORD GAMEPAD_X = 0x4000;
+const DWORD GAMEPAD_Y = 0x8000;
+
+struct ControllerState
+{
+    XINPUT_STATE state;
+    XINPUT_VIBRATION vibration;
+    float vibrateTimeLeft;
+    float vibrateTimeRight;
+    bool connected;
+};
+
+class Input
+{
+  private:
+    bool keysDown[inputNS::KEYS_ARRAY_LEN];       // true if specified key is down
+    bool keysPressed[inputNS::KEYS_ARRAY_LEN];    // true if specified key was pressed
+    std::string textIn;                           // user entered text
+    char charIn;                                  // last character entered
+    bool newLine;                                 // true on start of new line
+    int mouseX, mouseY;                           // mouse screen coordinates
+    int mouseRawX, mouseRawY;                     // high-definition mouse data
+    RAWINPUTDEVICE Rid[1];                        // for high-definition mouse
+    bool mouseCaptured;                           // true if mouse captured
+    bool mouseLButton;                            // true if left mouse button down
+    bool mouseMButton;                            // true if middle mouse button down
+    bool mouseRButton;                            // true if right mouse button down
+    bool mouseX1Button;                           // true if X1 mouse button down
+    bool mouseX2Button;                           // true if X2 mouse button down
+    ControllerState controllers[MAX_CONTROLLERS]; // state of controllers
+
+    const ControllerState *getControllerState(UINT n)
+    {
+        return &controllers[(n > MAX_CONTROLLERS - 1) ? MAX_CONTROLLERS - 1 : n];
+    }
+
+  public:
+    Input();
+    virtual ~Input();
+    void initialise(HWND, bool);
+    void keyDown(WPARAM);
+    void keyUp(WPARAM);
+    void keyIn(WPARAM);
+    bool isKeyDown(UCHAR) const;
+    bool wasKeyPressed(UCHAR) const;
+    void clearKeyPress(UCHAR);
+    void clear(UCHAR);
+    bool anyKeyPressed() const;
+    void clearAll() const { clear(inputNS::KEYS_MOUSE_TEXT); }
+    void cleartextIn() { this->textIn.clear(); }
+    std::string getTextIn() { return this->textIn; }
+    char getCharIn() { return this->charIn; }
+
+    void mouseIn(LPARAM);
+    void mouseRawIn(LPARAM);
+
+    void setMouseLButton(bool b) { this->mouseLButton = b; }
+    void setMouseMButton(bool b) { this->mouseMButton = b; }
+    void setMouseRButton(bool b) { this->mouseRButton = b; }
+
+    void setMouseXButton(WPARAM wParam)
+    {
+        this->mouseX1Button = (bool)(wParam & MK_XBUTTON1);
+        this->mouseX2Button = (bool)(wParam & MK_XBUTTON2);
+    }
+
+    int getMouseX() const { return this->mouseX; }
+    int getMouseY() const { return this->mouseY; }
+
+    int getMouseRawX() const { return this->mouseRawX; }
+    int getMouseRawY() const { return this->mouseRawY; }
+
+    bool getMouseLButton() const { return mouseLButton; }
+    bool getMouseMButton() const { return mouseMButton; }
+    bool getMouseRButton() const { return mouseRButton; }
+    bool getMouseX1Button() const { return mouseX1Button; }
+    bool getMouseX2Button() const { return mouseX2Button; }
+
+    void checkControllers();
+    void readControllers();
+
+    const WORD getGamepadButtons(UINT n)
+    {
+        return (*this->getControllerState(n)).state.Gamepad.wButtons;
+    }
+
+    // Match button bit pattern
+    bool getGamepadButtonState(UINT n, DWORD bitPattern)
+    {
+        return (this->getGamepadButtons(n) & bitPattern) != 0;
+    }
+
+    BYTE getGamepadLeftTrigger(UINT n)
+    {
+        return (*this->getControllerState(n)).state.Gamepad.bLeftTrigger;
+    }
+
+    BYTE getGamepadRightTrigger(UINT n)
+    {
+        return (*this->getControllerState(n)).state.Gamepad.bRightTrigger;
+    }
+
+    SHORT getGamePadThumbLX(UINT n)
+    {
+        return (*this->getControllerState(n)).state.Gamepad.sThumbLX;
+    }
+
+    SHORT getGamePadThumbLY(UINT n)
+    {
+        return (*this->getControllerState(n)).state.Gamepad.sThumbLY;
+    }
+
+    SHORT getGamePadThumbRX(UINT n)
+    {
+        return (*this->getControllerState(n)).state.Gamepad.sThumbRX;
+    }
+
+    SHORT getGamePadThumbRY(UINT n)
+    {
+        return (*this->getControllerState(n)).state.Gamepad.sThumbRY;
+    }
+
+    // Gamepad vibrate
+    // left: low freq, right: high freq
+    void gamePadVibrateLeft(UINT n, WORD speed, float sec)
+    {
+        if (n > MAX_CONTROLLERS - 1)
+            n = MAX_CONTROLLERS - 1;
+        this->controllers[n].vibration.wLeftMotorSpeed = speed;
+        this->controllers[n].vibrateTimeLeft = sec;
+    }
+
+    void gamePadVibrateRight(UINT n, WORD speed, float sec)
+    {
+        if (n > MAX_CONTROLLERS - 1)
+            n = MAX_CONTROLLERS - 1;
+        this->controllers[n].vibration.wRightMotorSpeed = speed;
+        this->controllers[n].vibrateTimeRight = sec;
+    }
+
+    void vibrateControllers(float frameTime);
+};
+
 #endif
--- src/input.cpp
+++ src/input.cpp
@@ -1,1 +1,197 @@
 #include "input.h"
+
+Input::Input()
+{
+    for (size_t i = 0; i < inputNS::KEYS_ARRAY_LEN; i++)
+    {
+
+        this->keyDown[i] = false;
+        this->keysPressed[i] = false;
+    }
+    this->newLine = true;
+    this->textIn = "";
+    this->charIn = 0;
+
+    this->mouseX, this->mouseY, this->mouseRawX, this->mouseRawY = 0;
+
+    this->mouseLButton, this->mouseMButton, this->mouseRButton, this->mouseX1Button, this->mouseX2Button = false;
+
+    for (int i = 0; i < MAX_CONTROLLERS; i++)
+        this->controllers[i].vibrateTimeLeft, this->controllers[i].vibrateTimeRight = 0;
+}
+
+Input::~Input()
+{
+    if (this->mouseCaptured)
+        ReleaseCapture();
+}
+
+void Input::initialise(HWND hwnd, bool capture)
+{
+    try
+    {
+        this->mouseCaptured = capture;
+
+        this->Rid[0].usUsagePage = HID_USAGE_PAGE_GENERIC;
+        this->Rid[0].usUsage = HID_USAGE_GENERIC_MOUSE;
+        this->Rid[0].dwFlags = RIDEV_INPUTSINK;
+        this->Rid[0].hwndTarget = hwnd;
+
+        RegisterRawInputDevices(Rid, 1, sizeof(Rid[0]));
+
+        if (this->mouseCaptured)
+            SetCapture(hwnd);
+
+        // Clear controller memory
+        ZeroMemory(this->controllers, sizeof(ControllerState), *MAX_CONTROLLERS);
+        this->checkControllers();
+    }
+    catch (...)
+    {
+        throw(GameError(gameErrorNS::FATAL_ERROR, "Error initialising input system"));
+    }
+}
+
+void Input::keyDown(WPARAM wParam)
+{
+    if (wParam < inputNS::KEYS_ARRAY_LEN)
+        this->keysDown[wParam], this->keysPressed[wParam] = true;
+}
+
+void Input::keyUp(WPARAM wParam)
+{
+    if (wParam < inputNS::KEYS_ARRAY_LEN)
+        this->keysDown[wParam] = false;
+}
+
+void Input::keyIn(WPARAM wParam)
+{
+    if (this->newLine)
+    {
+        this->textIn.clear();
+        this->newLine = false;
+    }
+
+    if (wParam == '\b' && this->textIn.length() > 0)
+    {
+        this->textIn.erase(this->textIn.size() - 1);
+    }
+    else
+    {
+        this->textIn += wParam;
+        this->charIn = wParam;
+    }
+
+    this->newLine = (char)wParam == '/r';
+}
+
+bool Input::isKeyDown(UCHAR vKey) const
+{
+    if (vKey < inputNS::KEYS_ARRAY_LEN)
+        return this->keysDown[vKey];
+
+    return false;
+}
+
+bool Input::wasKeyPressed(UCHAR vKey) const
+{
+    if (vKey < inputNS::KEYS_ARRAY_LEN)
+        return this->keysPressed[vKey];
+    return false;
+}
+
+bool Input::anyKeyPressed() const
+{
+    for (size_t i = 0; i < inputNS::KEYS_ARRAY_LEN; i++)
+    {
+        if (this->keysPressed[i])
+            return true;
+    }
+    return false;
+}
+
+void Input::clearKeyPress(UCHAR vKey)
+{
+    if (vKey < inputNS::KEYS_ARRAY_LEN)
+        this->keysPressed[vKey] = false;
+}
+
+void Input::clear(UCHAR vKey)
+{
+    using namespace inputNS;
+
+    if (vKey & KEYS_DOWN)
+    {
+        for (size_t i = 0; i < KEYS_ARRAY_LEN; i++)
+            this->keysDown[i] = false;
+    }
+    if (vKey & KEYS_PRESSED)
+    {
+        for (size_t i = 0; i < KEYS_ARRAY_LEN; i++)
+            this->keysPressed[i] = false;
+    }
+    if (vKey & MOUSE)
+    {
+        this->mouseX, this->mouseY, this->mouseRawX, this->mouseRawY = 0;
+    }
+    if (vKey & TEXT_IN)
+    {
+        this->cleartextIn();
+    }
+}
+
+void Input::mouseIn(LPARAM lParam)
+{
+    this->mouseX = GET_X_LPARAM(lParam);
+    this->mouseY = GET_Y_LPARAM(lParam);
+}
+
+// High-def mouse
+void Input::mouseRawIn(LPARAM lParam)
+{
+    UINT dwSize = 40;
+    static BYTE lpb[40];
+
+    GetRawInputData((HRAWINPUT)lParam, RID_INPUT, lpb, &dwSize, sizeof(RAWINPUTHEADER));
+
+    RAWINPUT *raw = (RAWINPUT *)lpb;
+
+    if (raw->header.dwType == RIM_TYPEMOUSE)
+    {
+        this->mouseRawX = raw->data.mouse.lLastX;
+        this->mouseRawY = raw->data.mouse.lLastY;
+    }
+}
+
+void Input::checkControllers()
+{
+    for (DWORD i = 0; i < MAX_CONTROLLERS; i++)
+    {
+        this->controllers[i].connected = XInputGetState(i, &controllers[i].state) == ERROR_SUCCESS;
+    }
+}
+
+void Input::readControllers()
+{
+    for (DWORD i = 0; i < MAX_CONTROLLERS; i++)
+    {
+        if (this->controllers[i].connected && XInputGetState(i, &controllers[i].state == ERROR_DEVICE_NOT_CONNECTED))
+            this->controllers[i].connected = false;
+    }
+}
+
+void Input::vibrateControllers(float frameTime)
+{
+    for (int i = 0; i < MAX_CONTROLLERS; i++)
+    {
+        if (this->controllers[i].connected)
+        {
+            this->controllers[i].vibrateTimeLeft -= frameTime;
+            if (this->controllers[i].vibrateTimeLeft < 0)
+                this->gamePadVibrateLeft(i, 0, 0);
+            this->controllers[i].vibrateTimeRight -= frameTime;
+            if (this->controllers[i].vibrateTimeRight < 0)
+                this->gamePadVibrateRight(i, 0, 0);
+        }
+    }
+}
