8348d9216d006fd4fd703e2218f335ddff063dc8
Sean Lim <lim_kai_xiang_sean@s2013.sst.edu.sg>
2019-01-07 00:46:16 +0800

    Add component flags, system list class

--- ecs/system.h
+++ ecs/system.h
@@ -1,23 +1,59 @@
 #pragma once
 
+#include "array.h"
 #include "component.h"
-#include "vector"
 
 class System
 {
 public:
-  System(const std::vector<unsigned int>& _componentTypes)
-      : componentTypes(_componentTypes)
-  {
-  }
-
+  enum {
+    FLAG_OPTIONAL = 1,
+  };
+  System() {}
+  const Array<unsigned int>& getComponentTypes() { return componentTypes; }
+  const Array<unsigned int>& getComponentFlags() { return componentFlags; }
   virtual void updateComponents(float delta, BaseComponent** components) {}
 
-  const std::vector<unsigned int> getComponentTypes()
+  bool isValid()
   {
-    return this->componentTypes;
+    for (int i = 0; i < componentFlags.size(); i++) {
+      if ((componentFlags[i] & System::FLAG_OPTIONAL) == 0) return true;
+    }
+    return false;
   }
 
 private:
-  std::vector<unsigned int> componentTypes;
+  Array<unsigned int> componentTypes;
+  Array<unsigned int> componentFlags;
 };
+
+class SystemList
+{
+public:
+  inline bool addSystem(System& system)
+  {
+    if (!system.isValid()) return false;
+
+    systems.push_back(&system);
+    return true;
+  }
+
+  inline size_t size() { return systems.size(); }
+
+  inline System* operator[](unsigned int index) { return systems[index]; }
+
+  bool removeSystem(System& system)
+  {
+    for (int i = 0; i < systems.size(); i++) {
+      if (&system == systems[i]) {
+        systems.erase(systems.begin() + i);
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+private:
+  Array<System*> systems;
+}
